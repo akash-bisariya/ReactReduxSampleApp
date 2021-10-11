1631f534a83774cd7083176c8ff9ad479f31ecfb
akash-bisariya <bigakash@gmail.com>
2021-10-11 20:47:36 +0530

    Added onchange event on state and district select

--- src/components/register/RegisterFormFn.js
+++ src/components/register/RegisterFormFn.js
@@ -2,27 +2,62 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import './Form.css'
 import axios from 'axios'
-
+import { useState } from "react";
 
 export default function RegisterFormFN() {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
 
-    const onSubmit = data => axios.post("http://localhost:3000/users",data)
-    .then(function(response){
-        if(response.data)
-        alert("User has been registered!! Please login.")
-    })
-    .catch(function (error) {
-        // handle error
-        alert("Server Issue - "+error);
-    });;
+    const onSubmit = data => axios.post("http://localhost:3000/users", data)
+        .then(function (response) {
+            if (response.data)
+                alert("User has been registered!! Please login.")
+        })
+        .catch(function (error) {
+            // handle error
+            alert("Server Issue - " + error);
+        });;
 
-    // console.log(data.dob);
+    const [districts, setDistrict] = useState([
+        <option value="">Select District</option>,
+        <option value="Gurgaon">Gurgaon</option>,
+        <option value="Amritsar">Amritsar</option>,
+        <option value="Jaipur">Jaipur</option>,
+        <option value="Meerut">Meerut</option>,
+        <option value="Gurgaon">Gurgaon</option>
+    ]);
+
+    function handleChange(event){
+      
+        console.log(event.target.value);
+        switch (event.target.value) {
+            case "Haryana": {
+                setDistrict(<option value="Gurgaon">Gurgaon</option>)
+                break;
+            }
+            case "Punjab": {
+                setDistrict(<option value="Amritsar">Amritsar</option>)
+                break;
+            }
+            case "Rajasthan": {
+                setDistrict(<option value="Jaipur">Jaipur</option>)
+                break;
+            }
+            case "Uttar-Pradesh": {
+                setDistrict(<option value="Meerut">Meerut</option>)
+                break;
+            }
+            default: {
+                setDistrict(<option value="Gurgaon">Gurgaon</option>)
+                break;
+            }
+        }
+        event.preventDefault();
+    };
 
     console.log(watch("example"));
     return (
         <form onSubmit={handleSubmit(onSubmit)} className='form-input'>
-            <h1 style={{color:'#15cdfc', textShadow:'2px 2px'}}>Bank Account Register</h1>
+            <h1 style={{ color: '#15cdfc', textShadow: '2px 2px' }}>Bank Account Register</h1>
             <div className='form-label'>
                 <input placeholder="Username" {...register("username", {
                     required: true, minLength: {
@@ -65,12 +100,12 @@ export default function RegisterFormFN() {
             </div>
             <div className='form-label'>
                 <label>Select State : </label>
-                <select {...register("state", { required: true })}>
+                <select onChange={handleChange} {...register("state", { required: true })}>
                     <option value="">Select State</option>
                     <option value="Haryana">Haryana</option>
                     <option value="Punjab">Punjab</option>
                     <option value="Rajasthan">Rajasthan</option>
-                    <option value="Uttar Pradesh">Uttar Pradesh</option>
+                    <option value="Uttar Pradesh">Uttar-Pradesh</option>
                 </select>
                 {errors.state && <div className='form-error'>State is required</div>}
 
@@ -78,11 +113,12 @@ export default function RegisterFormFN() {
             <div className='form-label'>
                 <label>Select City : </label>
                 <select {...register("district", { required: true })}>
-                    <option value="">Select District</option>
+                    {/* <option value="">Select District</option>
                     <option value="Amritsar">Amritsar</option>
-                    <option value="Gurgaon">Punjab</option>
+                    <option value="Gurgaon">Gurgaon</option>
                     <option value="Meerut">Meerut</option>
-                    <option value="Jaipur">Jaipur</option>
+                    <option value="Jaipur">Jaipur</option> */}
+                    {districts}
                 </select>
                 {errors.district && <div className='form-error'>District is required</div>}
 
@@ -112,7 +148,7 @@ export default function RegisterFormFN() {
                 {errors.confirmPassword && <div className='form-error'>Minimum 4 & Max 10 length allowed.</div>}
             </div>
             <div className='form-label'>
-                <input type="hidden" value="0" {...register("balance" )} />
+                <input type="hidden" value="0" {...register("balance")} />
                 {errors.balance && <div className='form-error'>Minimum 100 & Maximum 10000 amount can be added.</div>}
             </div>
 
